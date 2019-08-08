efad837ac270deb8b4268afe90c7fc73151096a3
Rob McBryde <robertmcbryde83@gmail.com>
2019-08-08 17:09:46 +0100

    refactor to add empty string to possible path of root in addition to forward slash

--- src/main/java/com/woita/jokeapp/controllers/JokesController.java
+++ src/main/java/com/woita/jokeapp/controllers/JokesController.java
@@ -1,6 +1,7 @@
 package com.woita.jokeapp.controllers;
 
 import com.woita.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +14,13 @@ public class JokesController {
 
     private JokeService jokeService;
 
+    @Autowired
     public JokesController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping("/")
-    public String getJoke(Model model) {
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
         model.addAttribute("joke", this.jokeService.getJoke());
         return "chuckNorris";
     }
